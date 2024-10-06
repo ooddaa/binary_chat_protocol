8ec622ad1a7db65b18ba9c52bfdc6bb8b4a269f2
oda <ooddaa@gmail.com>
2024-10-06 17:00:43 +0100

    add encode/decode for Register/Broadcast msgs, test

--- lib/chat/protocol.ex
+++ lib/chat/protocol.ex
@@ -3,17 +3,33 @@ defmodule Chat.Protocol do
 
   @type message() :: Broadcast.t() | Register.t()
 
-  @spec decode_message(binary()) :: {:ok, message(), binary()} | :error | :incomplete
+  @spec encode_message(binary()) :: {:ok, message(), binary()} | {:error, :not_implemented, metadata()} | {:error, :incomplete, metadata()}
   def decode_message(<<0x01, rest::binary>>), do: decode_register(rest)
   def decode_message(<<0x02, rest::binary>>), do: decode_broadcast(rest)
-  def decode_message(<<>>), do: :incomplete
-  def decode_message(<<_::binary>>), do: :error
+  def decode_message(<<>>), do: {:error, :incomplete, {__ENV__.function, <<>>}}
+  def decode_message(msg), do: {:error, :not_implemented, {__ENV__.function, msg}}
 
   defp decode_register(<<username_len::16, username::binary-size(username_len), rest::binary>>), do: {:ok, %Register{ username: username }, rest }
   
-  defp decode_register(<<_::binary>>), do: :incomplete
+  defp decode_register(<<msg::binary>>), do: {:error, :incomplete, {__ENV__.function, msg}}
 
   defp decode_broadcast(<<username_len::16, username::binary-size(username_len), contents_len::16, contents::size(contents_len)-binary, rest::binary>>), do: {:ok, %Broadcast{ username: username, contents: contents }, rest }
   
-  defp decode_broadcast(<<_::binary>>), do: :incomplete
+  defp decode_broadcast(<<msg::binary>>), do: {:error, :incomplete, {__ENV__.function, msg}}
+
+  @type metadata::{{atom(), non_neg_integer()}, any()} 
+  @spec encode_message(message()) :: {:ok, binary()} | {:error, :not_implemented, metadata()} | {:error, :incomplete, metadata()}
+  def encode_message(%Register{} = msg), do: encode_register(msg)
+  def encode_message(%Broadcast{} = msg), do: encode_broadcast(msg)
+  def encode_message(msg), do: {:error, :not_implemented, {__ENV__.function, msg}}
+
+  defp encode_register(%Register{ username: username }) when is_binary(username) and byte_size(<<username::binary>>) > 1 do
+    <<0x01, byte_size(username)::size(16), username::binary>>  
+  end
+  defp encode_register(msg), do: {:error, :incomplete, {__ENV__.function, msg}}
+
+  defp encode_broadcast(%Broadcast{ username: username, contents: contents}) when is_binary(username) and byte_size(<<username::binary>>) > 1 and is_binary(contents) and byte_size(<<contents::binary>>) > 1 do
+    <<0x02, byte_size(username)::size(16), username::binary, byte_size(contents)::size(16), contents::binary>>  
+  end
+  defp encode_broadcast(msg), do: {:error, :incomplete, {__ENV__.function, msg}}
 end
--- test/protocol_test.exs
+++ test/protocol_test.exs
@@ -5,14 +5,14 @@ defmodule Chat.ProtocolTest do
   alias Chat.Messages.{Broadcast, Register}
 
   describe "decode_message/1" do
-    test "can decode register messages" do
+    test "can decode Register messages" do
       username = "oda"
       binary = <<0x01, 0x00, 0x03, username::binary, "rest">>
       assert {:ok, msg, rest} = Protocol.decode_message(binary)
       assert msg == %Register{ username: username }
       assert rest == "rest"
 
-      assert Chat.Protocol.decode_message(<<0x01, 0x00>>)== :incomplete 
+      assert Protocol.decode_message(<<0x01, 0x00>>)== {:error, :incomplete, {{:decode_register, 1}, <<0x00>>}} 
     end
 
     test "can decode broadcast messages" do
@@ -23,15 +23,44 @@ defmodule Chat.ProtocolTest do
       assert msg == %Broadcast{ username: username, contents: contents }
       assert rest == "rest"
 
-      assert Chat.Protocol.decode_message(<<0x02, 0x00>>) == :incomplete 
+      assert Protocol.decode_message(<<0x02, 0x00>>) == {:error, :incomplete, {{:decode_broadcast, 1}, <<0x00>>}} 
     end
 
     test "returns :incomplete for empty data" do
-      assert Chat.Protocol.decode_message("") == :incomplete
+      assert Protocol.decode_message("") == {:error, :incomplete, {{:decode_message, 1}, ""}}
     end
 
-    test "returns :error for unknown message types" do
-      assert Chat.Protocol.decode_message(<<0x03, 0x00>>) == :error
+    test "returns :not_implemented for unknown message types" do
+      msg = <<0x03, 0x00>>
+      assert Protocol.decode_message(msg) == {:error, :not_implemented, {{:decode_message, 1}, msg}}
+    end
+  end
+  
+  describe "encode_message/1" do
+    test "can encode Register messages" do
+      msg = %Register{ username: "oda" }
+      assert Protocol.encode_message(msg) == <<0x01, 0x00, 0x03, "oda">> 
+    end
+    
+    test "can encode Broadcast messages" do
+      msg = %Broadcast{ username: "oda", contents: "elixir rocks" }
+      assert Protocol.encode_message(msg) == <<0x02, 0x00, 0x03, "oda", 0x00, 0x0c, "elixir rocks">> 
+    end
+    
+    test "returns :incomplete for empty data" do
+      register_msg = %Register{ username: "" }
+      assert Protocol.encode_message(register_msg) == {:error, :incomplete, {{:encode_register, 1}, register_msg}}
+      broadcast_msg = %Broadcast{ username: "", contents: "" }
+      assert Protocol.encode_message(broadcast_msg) == {:error, :incomplete, {{:encode_broadcast, 1}, broadcast_msg}}
+      broadcast_msg = %Broadcast{ username: "oda", contents: "" }
+      assert Protocol.encode_message(broadcast_msg) == {:error, :incomplete, {{:encode_broadcast, 1}, broadcast_msg}}
+      broadcast_msg = %Broadcast{ username: "", contents: "contents" }
+      assert Protocol.encode_message(broadcast_msg) == {:error, :incomplete, {{:encode_broadcast, 1}, broadcast_msg}}
+    end
+
+    test "returns :not_implemented for unknown message types" do
+      msg = ""
+      assert Protocol.encode_message(msg) == {:error, :not_implemented, {{:encode_message, 1}, msg}}
     end
   end
 end
